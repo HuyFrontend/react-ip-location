306a59e857afac22c77251b3aea1a45669638729
Huy Vo <huy.voxuan@carro.co>
2023-05-15 08:07:57 +0800

    update version

--- src/services/ipLocation.ts
+++ src/services/ipLocation.ts
@@ -1,16 +1,17 @@
-import axios from "axios";
-
 const ipLocation = async (ipAddress?: string) => {
   const countryIS = ipAddress
     ? `https://api.country.is/${ipAddress}`
     : `https://api.country.is/`;
-  const res = await axios.get(countryIS)?.then((resrult: any) => {
-    return resrult.data;
-  });
 
+  const res = await fetch(countryIS)
+    .then((res) => res.json())
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => console.error(`Error:`, error));
   // const res = await axios.get('https://api.country.is/101.110.63.255'); // JP test
   // const res = await axios.get('https://api.country.is/101.109.255.255'); // TH
-
+  console.log("REAS", res);
   return res || null;
 };
 
